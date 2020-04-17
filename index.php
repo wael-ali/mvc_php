c4e230ac15dde62076fb02cbe84caa9f9288a136
Weal Ali <wealali595@gmail.com>
2020-04-17 14:56:37 +0200

    add .env to project + save value in $_GLOBALS

--- index.php
+++ index.php
@@ -15,8 +15,34 @@ define('DATA', APP.'Data'.DS);
 define('CORE', APP.'Core'.DS);
 define('SERVICES', APP.'Service'.DS);
 define('CONTROLLER', APP.'Controller'.DS);
+define('DOT_ENV', ROOT.'.env');
 define('CONTROLLERS_NAME_SPACE', '\app\Controller\\');
 define('SERVICES_NAME_SPACE', '\app\Service\\');
+
+function parseDoteEnv(){
+    if (file_exists(DOT_ENV)){
+        $envArray = [];
+        $fileLines = file(DOT_ENV);
+        foreach ($fileLines as $fileLine){
+            // escape empty lines
+            $fileLine = trim($fileLine);
+            if ($fileLine == ''){
+                continue;
+            }
+
+            if (!preg_match('/^[A-Z][A-Z,_]+=([^\s])+$/',$fileLine)){
+                continue;
+            }
+            $varValue = explode('=',$fileLine);
+            $envArray[$varValue[0]] = $varValue[1];
+        }
+        foreach ($envArray as $var => $value){
+            $GLOBALS[$var] = $value;
+        }
+        return 1;
+    }
+    throw new Exception('.env file is not found in app root!.');
+}
 
 function dd(){
     $params = func_get_args();
@@ -35,6 +61,7 @@ spl_autoload_register(function ($className){
     }
 });
 // TODO PARSING CONFIGERATION FROM .env FILE TO GLOBAL VARIABLES
+parseDoteEnv(ROOT.'.env');
 
 $container = new Container(new DbConnection([
     'host' => 'localhost',
